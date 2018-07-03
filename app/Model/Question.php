b7a6c3a0d0326000ad96870afb3d7a1f22a847c9
Dear <unisexx@gmail.com>
2018-07-03 15:48:58 +0700

    CRUD for Question for along with Question Resource Transformation

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,15 +7,31 @@ use App\User;
 
 class Question extends Model
 {
-    public function user(){
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    // protected $fillable = ['title','slug','body','category_id','user_id'];
+    protected $guarded = []; // ถ้าเป็นแบบนี้แสดงว่าบันทึกลงทุกช่อง เพราะไม่ได้ ignore ฟิลด์ไหนเลย
+
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function replies(){
+    public function replies()
+    {
         return $this->hasMany(Reply::class);
     }
 
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\QuestionController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// use Illuminate\Http\Request;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// /*
+// |--------------------------------------------------------------------------
+// | API Routes
+// |--------------------------------------------------------------------------
+// |
+// | Here is where you can register API routes for your application. These
+// | routes are loaded by the RouteServiceProvider within a group which
+// | is assigned the "api" middleware group. Enjoy building your API!
+// |
+// */
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::apiResource('/question','QuestionController');
