9a4e6b4f551b5afcf7149f532bf35b066debc9b2
Dear <unisexx@gmail.com>
2018-07-25 16:00:11 +0700

    vuetify

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,10 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+ 
+Vue.use(Vuetify)
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,7 +19,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('AppHome', require('./components/AppHome.vue'));
 
 const app = new Vue({
     el: '#app'
